f6a299cf4647cfbad718534e4d7660e23b35413d
nohder <noh.sayachack@gmail.com>
2023-06-22 11:16:09 +0700

    remove unused

--- src/main/java/org/ib/signer/App.java
+++ src/main/java/org/ib/signer/App.java
@@ -3,17 +3,10 @@ package org.ib.signer;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
-import java.sql.SQLOutput;
-import java.util.HashMap;
-
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         SignatureManager signatureManager = new SignatureManager();
         String signature;
-
         Gson gson = new Gson();
 
         if (args.length == 3) {
@@ -29,38 +22,8 @@ public class App {
                 default -> System.out.println("default");
             }
 
-
         } else {
             System.out.println("invalid args");
-
-//            System.out.println("===> sign");
-//            String aaa = "{\n" +
-//                    "    \"merchantId\": \"KKL\",\n" +
-//                    "    \"mobileNo\":       \"2055555555x\"\n" +
-//                    "}";
-//            JsonObject jsonObject = new Gson().fromJson(aaa, JsonObject.class);
-//            System.out.println("jsonObject: " + jsonObject.toString());
-//            signature = signatureManager.genSignatureWithPrivate(jsonObject.toString(), "private.key");
-//
-//            System.out.println("===> verify");
-//
-//            HashMap<String, String> body = new HashMap<>();
-//
-//            // Add keys and values to the HashMap
-//            body.put("merchantId", "KKL");
-//            body.put("mobileNo", "2055555555x");
-//            System.out.println("body: " + body.toString());
-//
-//            System.out.println("signature: " + signature);
-//            JsonObject jsonObject2 = new Gson().fromJson(body.toString(), JsonObject.class);
-//            System.out.println("jsonObject2: " + jsonObject2.toString());
-//
-//            boolean isVerifyOk = signatureManager.verifySignaturePub(jsonObject2.toString(), signature, "public.key");
-//            System.out.println("isVerifyOk: " + isVerifyOk);
-
         }
-
-
-
     }
 }
