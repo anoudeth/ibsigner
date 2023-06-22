c2f7e5397cb4d92805463ed27c146fb5116418c5
nohder <noh.sayachack@gmail.com>
2023-06-22 16:09:59 +0700

    remove unused

--- src/main/java/org/ib/signer/App.java
+++ src/main/java/org/ib/signer/App.java
@@ -7,26 +7,12 @@ import java.util.HashMap;
 
 public class App {
     public static void main(String[] args) {
-        SignatureManager signatureManager = new SignatureManager();
-        String signature;
-        Gson gson = new Gson();
 
         if (args.length == 1) {
-
-            JsonObject jsonObject = gson.fromJson(args[0], JsonObject.class);
-            signature = signatureManager.genSignatureWithPrivate(jsonObject.toString(), "private.key");
+            SignatureManager signatureManager = new SignatureManager();
+            JsonObject jsonObject = new Gson().fromJson(args[0], JsonObject.class);
+            String signature = signatureManager.genSignatureWithPrivate(jsonObject.toString(), "private.key");
             System.out.println(signature);
-
-//            switch (args[0]) {
-//                case "SIGN" -> {
-//                    JsonObject jsonObject = gson.fromJson(data, JsonObject.class);
-//                    System.out.println("jsonObject: " + jsonObject.toString());
-//                    signature = signatureManager.genSignatureWithPrivate(jsonObject.toString(), privateKey);
-//                    System.out.println(signature);
-//                }
-//                case "VERIFY" -> System.out.println("VERIFY");
-//                default -> System.out.println("invalid args");
-//            }
 
         } else {
             System.out.println("invalid args");
